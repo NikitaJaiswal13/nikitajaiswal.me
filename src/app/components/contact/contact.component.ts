b6a8b6f9aa496aba8365db29a8b3d58d32e14560
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2025-03-25 15:10:17 +0530

    Email Done

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -16,20 +17,45 @@ export class ContactComponent implements OnInit {
       {
         name : ['' , [Validators.required , Validators.minLength(3)]],
         email : ['' , [Validators.required , Validators.email]],
-        message : ['' , [Validators.required , Validators.minLength(5)]]
+        message : ['' , [Validators.required ]]
       }
     )
    }
 
    onSubmit() {
-    if(this.contactForm.valid){
-      console.log("Form Submitted " , this.contactForm.value);
-      alert("Message has been sent SuccessFully")
-      this.contactForm.reset()
+    if (this.contactForm.valid) {
+      // console.log("Form Submitted ", this.contactForm.value);
+  
+      const templateParams = {
+        name: this.contactForm.value.name,
+        email: this.contactForm.value.email,
+        message: this.contactForm.value.message,
+        title: 'New Message',
+        time: new Date().toLocaleTimeString()
+      };
+  
+      // console.log("Template Params: ", templateParams);
+  
+      emailjs.send('service_fjz59lt', 'template_muvvbzn', templateParams, 'mrOnINU4qNfEzcCCN')
+        .then((response) => {
+          console.log('Email sent!', response);
+          alert('Message sent successfully!');
+          this.contactForm.reset();
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+          alert('Failed to send message. Please try again.');
+        });
+    } else {
+      alert("Please fill all required fields correctly.");
     }
-   }
+  }
+  
+
 
   ngOnInit(): void {
     
   }
+
+
 }
